07ce22e70728dc3092662508f185bb01145f169b
MichelH7cker <michel.hecker@usp.br>
2025-06-28 19:21:28 -0300

    feat: create basic frontend design

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import SearchVisual from "./components/SearchBar";
+
+function App() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
+      
+      <div className="w-full max-w-2xl">
+        <SearchVisual />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
