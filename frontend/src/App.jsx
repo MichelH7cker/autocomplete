e2704b33a5d83c0195a7088194ac033641d377ca
MichelH7cker <michel.hecker@usp.br>
2025-06-27 22:48:02 -0300

    chore: create initial App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,28 +7,18 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-slate-100 min-h-screen flex flex-col items-center pt-10 px-4">
+      <div className="w-full max-w-xl">
+        <header className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-slate-800">Busca com autocompletar</h1>
+          <p className="text-slate-600 mt-2">Encontre sugestões enquanto digita. Digite no campo abaixo</p>
+        </header>
+
+        <main className="relative">
+          oi 
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
