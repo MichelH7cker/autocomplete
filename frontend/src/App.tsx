90c5e1ae3ad8fa15413ac9925c2a31415b9c7144
MichelH7cker <michel.hecker@usp.br>
2025-06-28 21:09:25 -0300

    feat: connect frontend to backend via graphql (not working yet)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
-import SearchVisual from "./components/SearchBar";
+import SearchAutocomplete from "./components/SearchAutocomplete";
 
 function App() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
-      
       <div className="w-full max-w-2xl">
-        <SearchVisual />
+        <SearchAutocomplete />
       </div>
-
     </div>
   )
 }
