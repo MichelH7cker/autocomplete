322b7714dac3d9c71927566aa66be063ee85f80d
MichelH7cker <michel.hecker@usp.br>
2025-07-01 14:04:45 -0300

    feat: configure GraphQL URI via environment variable

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+const graphqlUri = import.meta.env.VITE_GRAPHQL_URI;
+
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: graphqlUri,
   cache: new InMemoryCache(),
 });
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ApolloProvider client={client}> 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider> 
-  </StrictMode>,
-)
+    </ApolloProvider>
+  </React.StrictMode>,
+);
