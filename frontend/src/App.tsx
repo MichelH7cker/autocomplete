b9706ae1ecff651dadc059dddcc12fb3899f5328
MichelH7cker <michel.hecker@usp.br>
2025-07-02 20:13:21 -0300

    feat: add a header with my personal informations XD

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import SearchAutocomplete from "./components/SearchAutocomplete";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
-      <div className="w-full max-w-2xl">
-        <SearchAutocomplete />
-      </div>
+    <div className="w-full min-h-screen bg-gray-100 dark:bg-gray-900">
+      <Header />
+      <main className="w-full min-h-screen flex items-center justify-center p-4">
+        <div className="w-full max-w-2xl">
+          <SearchAutocomplete />
+        </div>
+      </main>
+
     </div>
   )
 }
