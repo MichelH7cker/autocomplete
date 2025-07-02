c11a9e34d8193892f5f668afed7fdf0c2602b112
MichelH7cker <michel.hecker@usp.br>
2025-07-01 21:04:36 -0300

    feat: application works locally and in render.com

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const graphqlUri = import.meta.env.VITE_GRAPHQL_URI;
@@ -17,4 +17,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </ApolloProvider>
   </React.StrictMode>,
-);
+)
